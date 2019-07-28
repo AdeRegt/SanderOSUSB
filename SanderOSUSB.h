02c04f1b88fe1934d1139b323da26c74dcda902f
AdeRegt <AdeRegt@github.com>
2019-07-28 10:37:49 +0200

    adding more to api

--- SanderOSUSB.h
+++ SanderOSUSB.h
@@ -6,7 +6,7 @@ void exit (void){
     	);
 }
 
-void print (const char* message,int to){
+void print(const char* message,int to){
 	int mode = 4;
 	int len = 0;
 	while(message[len++]!=0x00);
@@ -17,4 +17,30 @@ void print (const char* message,int to){
         , "+c" (message)
         , "+d" (len)
     	);
+}
+
+void read(int to,char *message,int len){
+	int mode = 3;
+	__asm__ __volatile__ (
+        "int $0x80"
+        : "+a" (mode)
+        , "+b" (to)
+        , "+c" (message)
+        , "+d" (len)
+    	);
+}
+
+int open(const char *pathname, int flags, int mode){
+	int model = 5;
+	int res = 0;
+	__asm__ __volatile__ (
+        "int $0x80"
+        : "+a" (model)
+        , "+b" (pathname)
+        , "+c" (flags)
+        , "+d" (mode)
+        :
+        "r" (res)
+    	);
+    	return res;
 }
