8a237161a372f8ad1e38741f07de47574c4d342b
AdeRegt <AdeRegt@github.com>
2019-11-05 23:05:15 +0100

    new attempt

--- kernel/dev/xhci.c
+++ kernel/dev/xhci.c
@@ -4,14 +4,23 @@ unsigned long xhci_base = 0;
 unsigned long xhci_usbcmd = 0;
 unsigned long xhci_usbsts = 0;
 unsigned long xhci_ports_offset = 0;
+unsigned long xhci_config = 0;
+unsigned long xhci_pagesize = 0;
+unsigned long xhci_dnctrl = 0;
 
 extern void xhciirq();
 
 void dumpPortstati(){
 	for(int i = 0 ; i < 10 ; i++){
 		unsigned long tar = ((unsigned char*)xhci_ports_offset+(i*0x10))[0];
-		if(tar & 1){
+		if(tar & 0b11){ // attached or connected?
+			printf("XHCI: PORT%x: %x attached \n",i,tar);
+		}else if(tar == 0x00){ // ignore
+		
+		}else if(tar != 0xA0){ // unknown status?
 			printf("XHCI: PORT%x: %x \n",i,tar);
+		}else { // force setpower on port
+			((unsigned char*)xhci_ports_offset+(i*0x10))[0] = 0b1000000000;
 		}
 	}
 }
@@ -41,59 +50,27 @@ void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
 	xhci_usbsts = xhci_base+0x04;
 	xhci_ports_offset = xhci_base+0x400;
 	printf("XHCI: USBCMD %x USBSTS %x \n",((unsigned long*)xhci_usbcmd)[0],((unsigned long*)xhci_usbsts)[0]);
-//	
-//	GETTING RIGHT OFFSETS
-	// is this offset already right?
-	if(((unsigned long*)xhci_usbsts)[0]==1){
-		// yes, do not change
-		printf("XHCI: default settings are OK\n");
-	}else{
-		bar = getBARaddress(bus,slot,function,0x10);
-		printf("XHCI: bar %x \n",bar);
-		capabilityregistersizeloc = bar;
-		capabilityregistersize = ((unsigned char*)capabilityregistersizeloc)[0];
-		printf("XHCI: caplength %x \n",capabilityregistersize);
-		xhci_base = capabilityregistersize+capabilityregistersizeloc;
-		printf("XHCI: registers at %x \n",xhci_base);
-		xhci_usbcmd = xhci_base;
-		xhci_usbsts = xhci_base+0x04;
-		xhci_ports_offset = xhci_base+0x40C;
-		printf("XHCI: USBCMD %x USBSTS %x \n",((unsigned long*)xhci_usbcmd)[0],((unsigned long*)xhci_usbsts)[0]);
-		if(((unsigned long*)xhci_usbsts)[0]==1){
-			// yes, do not change
-			printf("XHCI: discoverymethod 2 is OK\n");
-		}else{
-			bar = getBARaddress(bus,slot,function,0x10);
-			printf("XHCI: bar %x \n",bar);
-			capabilityregistersizeloc = 0x80;
-			printf("XHCI: predefined offset %x \n",capabilityregistersizeloc);
-			xhci_base = bar+capabilityregistersizeloc;
-			printf("XHCI: registers at %x \n",xhci_base);
-			xhci_usbcmd = xhci_base;
-			xhci_usbsts = xhci_base+0x04;
-			xhci_ports_offset = xhci_base+0x40C;
-			printf("XHCI: USBCMD %x USBSTS %x \n",((unsigned long*)xhci_usbcmd)[0],((unsigned long*)xhci_usbsts)[0]);
-			if(((unsigned long*)xhci_usbsts)[0]==1){
-				// yes, do not change
-				printf("XHCI: discoverymethod 3 is OK\n");
-			}else{
-				printf("XHCI: cannot resolve settings\n");
-				return;
-			}
-		}
-	}
-	
+
 	//
-	//
-	// getting all addresses
-	((unsigned long*)xhci_usbcmd)[0] = 2;
+	// resetting
+	((unsigned long*)xhci_usbcmd)[0] |= 2;
 	resetTicks();
 	while(((unsigned long*)xhci_usbcmd)[0]){
 		if(getTicks()==10){
 			printf("XHCI: TIMEOUT\n");
-			for(;;);
+			break;
 		}
 	}
+	xhci_config = xhci_base + 0x38;
+	xhci_pagesize = xhci_base + 0x08;
+	xhci_dnctrl = xhci_base + 0x14;
+	
+	// set 5 ports
+	((unsigned long*)xhci_config)[0] = 5;
+	// report everything
+	((unsigned long*)xhci_dnctrl)[0] |= 0b00000000000000001111111111111111;
+	// run
+	((unsigned long*)xhci_usbcmd)[0] |= 1;
 	
 	printf("\n[READY]\n\n");
 	dumpPortstati();
