84499e45da2ec65936d3133620530f45682f87e3
AdeRegt <AdeRegt@github.com>
2019-11-12 22:34:17 +0100

    adding basic detection

--- kernel/dev/xhci.c
+++ kernel/dev/xhci.c
@@ -1,35 +1,14 @@
 #include "../kernel.h"
 
-unsigned long xhci_base = 0;
-unsigned long xhci_usbcmd = 0;
-unsigned long xhci_usbsts = 0;
-unsigned long xhci_ports_offset = 0;
-unsigned long xhci_config = 0;
-unsigned long xhci_pagesize = 0;
-unsigned long xhci_dnctrl = 0;
-
 extern void xhciirq();
-
-void dumpPortstati(){
-	for(int i = 0 ; i < 10 ; i++){
-		unsigned long tar = ((unsigned char*)xhci_ports_offset+(i*0x10))[0];
-		if(tar & 0b11){ // attached or connected?
-			printf("XHCI: PORT%x: %x attached \n",i,tar);
-		}else if(tar == 0x00){ // ignore
-		
-		}else if(tar != 0xA0){ // unknown status?
-			printf("XHCI: PORT%x: %x \n",i,tar);
-		}else { // force setpower on port
-			((unsigned char*)xhci_ports_offset+(i*0x10))[0] = 0b1000000000;
-		}
-	}
-}
 
 void irq_xhci(){
 	printf("XHCI: int fire\n");
-	printf("XHCI: USBCMD %x USBSTS %x \n",((unsigned long*)xhci_usbcmd)[0],((unsigned long*)xhci_usbsts)[0]);
-	dumpPortstati();
 }
+
+unsigned long basebar = 0;
+unsigned long usbcmd =0;
+unsigned long usbsts = 0;
 
 // offset intell xhci 0x47C
 void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
@@ -38,41 +17,55 @@ void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
 	setNormalInt(usbint,(unsigned long)xhciirq);
 //	
 //	GETTING BASIC INFO
-	unsigned long capabilityregister = getBARaddress(bus,slot,function,0x34) & 0x000000FF;
+	unsigned long deviceid = (getBARaddress(bus,slot,function,0) & 0xFFFF0000) >> 16;
 	unsigned long bar = getBARaddress(bus,slot,function,0x10);
-	printf("XHCI: capreg %x bar %x \n",capabilityregister,bar);
-	unsigned long capabilityregistersizeloc = bar+capabilityregister;
-	unsigned long capabilityregistersize = ((unsigned char*)capabilityregistersizeloc)[0];
-	printf("XHCI: caplength %x \n",capabilityregistersize);
-	xhci_base = capabilityregistersize+capabilityregistersizeloc;
-	printf("XHCI: registers at %x \n",xhci_base);
-	xhci_usbcmd = xhci_base;
-	xhci_usbsts = xhci_base+0x04;
-	xhci_ports_offset = xhci_base+0x400;
-	printf("XHCI: USBCMD %x USBSTS %x \n",((unsigned long*)xhci_usbcmd)[0],((unsigned long*)xhci_usbsts)[0]);
-
-	//
-	// resetting
-	((unsigned long*)xhci_usbcmd)[0] |= 2;
-	resetTicks();
-	while(((unsigned long*)xhci_usbcmd)[0]){
-		if(getTicks()==10){
-			printf("XHCI: TIMEOUT\n");
-			break;
+	if(deviceid==0x22B5){
+		printf("[XHCI] INTELL XHCI CONTROLLER\n");
+		basebar = bar+0x7C;
+	}else{
+		printf("[XHCI] UNKNOWN XHCI CONTROLLER %x \n",deviceid);
+		basebar = bar + ((unsigned char*)bar)[0];
+	}
+	usbcmd = basebar;
+	usbsts = basebar+0x04;
+	
+	unsigned long xhci_usbcmd = ((unsigned long*)usbcmd)[0];
+	unsigned long xhci_usbsts = ((unsigned long*)usbsts)[0];
+	if((xhci_usbcmd & 1)==1){
+		printf("[XHCI] controller already running! (%x)\n",xhci_usbcmd);
+		// ask controller to stop
+		resetTicks();
+		((unsigned long*)usbcmd)[0] &= ~1;
+		while(getTicks()<5);
+		xhci_usbcmd = ((unsigned long*)usbcmd)[0];
+		xhci_usbsts = ((unsigned long*)usbsts)[0];
+		if((xhci_usbsts & 1)==1&&(xhci_usbcmd & 1)==0){
+			printf("[XHCI] stopping of hostcontroller succeed\n");
+		}else{
+			printf("[XHCI] failed to halt hostcontroller %x %x \n",xhci_usbcmd,xhci_usbsts);
 		}
 	}
-	xhci_config = xhci_base + 0x38;
-	xhci_pagesize = xhci_base + 0x08;
-	xhci_dnctrl = xhci_base + 0x14;
 	
-	// set 5 ports
-	((unsigned long*)xhci_config)[0] = 5;
-	// report everything
-	((unsigned long*)xhci_dnctrl)[0] |= 0b00000000000000001111111111111111;
-	// run
-	((unsigned long*)xhci_usbcmd)[0] |= 1;
-	
-	printf("\n[READY]\n\n");
-	dumpPortstati();
+	//
+	// lets reset!
+	printf("[XHCI] Resetting XHCI \n");
+	resetTicks();
+	((unsigned long*)usbcmd)[0] |= 2;
+	while(getTicks()<5);
+	xhci_usbcmd = ((unsigned long*)usbcmd)[0];
+	xhci_usbsts = ((unsigned long*)usbsts)[0];
+	printf("[XHCI] Reset XHCI finished with USBCMD %x and USBSTS %x \n",xhci_usbcmd,xhci_usbsts);
+	if(xhci_usbsts & 0b10000){
+		printf("[XHCI] Portchange detected!\n");
+	}
+	printf("[XHCI] Probing ports....\n");
+	for(int i = 0 ; i < 10 ; i++){
+		unsigned long map = basebar + 0x400 + (i*0x10);
+		unsigned long val = ((unsigned long*)map)[0];
+		if(val&3){
+			printf("[XHCI] Port %x seems to have a connection\n",i);
+		}
+	}
+	printf("[XHCI] All finished!\n");
 	for(;;);
 }
