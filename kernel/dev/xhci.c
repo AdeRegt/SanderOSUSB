7b5d6358a1b2da71cad64dc031a70980e3fdaed1
AdeRegt <AdeRegt@github.com>
2019-11-19 22:54:43 +0100

    adding doorbells

--- kernel/dev/xhci.c
+++ kernel/dev/xhci.c
@@ -1,5 +1,12 @@
 #include "../kernel.h"
 
+typedef struct{
+	unsigned long bar1;
+	unsigned long bar2;
+	unsigned long bar3;
+	unsigned long bar4;
+}TRB;
+
 extern void xhciirq();
 
 void irq_xhci(){
@@ -12,6 +19,8 @@ unsigned long usbsts = 0;
 unsigned long config = 0;
 unsigned long bcbaap = 0;
 unsigned long crcr   = 0;
+unsigned long doorbel= 0;
+TRB ring[10];
 
 // offset intell xhci 0x47C
 void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
@@ -22,13 +31,17 @@ void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
 //	GETTING BASIC INFO
 	unsigned long deviceid = (getBARaddress(bus,slot,function,0) & 0xFFFF0000) >> 16;
 	unsigned long bar = getBARaddress(bus,slot,function,0x10);
+	unsigned long capdb = bar+0x14;
 	unsigned long hciparamadr = bar+0x04;
 	unsigned long hciparam1 = ((unsigned long*)hciparamadr)[0];
 	unsigned long portcount = hciparam1>>24;
 	printf("[XHCI] portcount %x \n",portcount);
+	doorbel = bar+((unsigned long*)capdb)[0];
+	printf("[XHCI] doorbell offset %x \n",doorbel);
 	if(deviceid==0x22B5){
 		printf("[XHCI] INTELL XHCI CONTROLLER\n");
 		basebar = bar+0x7C;
+		doorbel += 0xC;
 	}else if(deviceid==0xD){
 		printf("[XHCI] QEMU XHCI CONTROLLER\n");
 		unsigned long premature = bar + (getBARaddress(bus,slot,function,0x34) & 0x000000FF);
@@ -81,10 +94,9 @@ void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
 	btc = btc<<6;
 	((unsigned long*)bcbaap)[0] = (unsigned long)btc;
 	// COMMAND RING CONTROLL
-	unsigned long ring[10];
 	unsigned long zen = (unsigned long)&ring;
 	zen = zen<<6;
-	((unsigned long*)crcr)[0] |= (unsigned long)zen;
+	((unsigned long*)crcr)[0] = (unsigned long)zen;
 	
 	
 	((unsigned long*)usbcmd)[0] |= 1;
@@ -96,23 +108,69 @@ void init_xhci(unsigned long bus,unsigned long slot,unsigned long function){
 		printf("[XHCI] Portchange detected!\n");
 	}
 	printf("[XHCI] Probing ports....\n");
+	//
+	// First, see which ports are available
+	// USB3.0 will say automatically he is there.
+	// USB2.0 should be activated manually
+	//
+	// After this we should initialise it by getting its port number from the ring
+	unsigned int att = 0;
 	for(int i = 0 ; i < 5 ; i++){
 		unsigned long map = basebar + 0x400 + (i*0x10);
 		unsigned long val = ((unsigned long*)map)[0];
 		if(val&3){ // USB 3.0 does everything themselves
 			printf("[XHCI] Port %x has a USB3.0 connection\n",i);
+			att++;
 		}else{ // USB 2.0 however doesnt...
 			((unsigned long*)map)[0] = 0b1000010000; // activate power and reset port
 			resetTicks();
-			while(getTicks()<5);
+			while(getTicks()<2);
 			val = ((unsigned long*)map)[0];
 			if(val&3){
-				printf("[XHCI] Port %x has a USB2.0 connection\n",i);
+				printf("[XHCI] Port %x has a USB2.0 connection (%x)\n",i,val);
+				att++;
 			}
+		}
+		
+		if(val&3){
+			printf("[XHCI] Port %x is initialising....\n",i);
+			//
+			// getting port number
+			TRB trb;
+			trb.bar1 = 0;
+			trb.bar2 = 0;
+			trb.bar3 = 0;
+			trb.bar4 = 0b00000000000000000010010000000000;
+			
+			
+			unsigned long tox = (unsigned long)&trb;
+			tox = tox<<4;
+			
+			TRB trb2 = ring[0];
+			trb2.bar1 = tox;
+			trb2.bar2 = 0;
+			trb2.bar3 = 0;
+			trb2.bar4 = 0b00000000000000001100110000000000;
+			
+			// doorbel
+			unsigned long tingdongaddr = doorbel;
+			((unsigned long*)tingdongaddr)[0] = 0;
+			
+			// wait
+			resetTicks();
+			while(getTicks()<2);
+			
+			// result
+			printf("[XHCI] %x %x %x %x \n",trb.bar1,trb.bar2,trb.bar3,trb.bar4);
+			printf("[XHCI] %x %x %x %x \n",trb2.bar1,trb2.bar2,trb2.bar3,trb2.bar4);
 		}
 	}
 	
-	printf("[XHCI] current crcr status %x \n",((unsigned long*)crcr)[0]);
+	if(((unsigned long*)crcr)[0]==0x8){
+		printf("[XHCI] circulair command ring is running\n");
+	}
 	printf("[XHCI] All finished!\n");
-	for(;;);
+	if(att>0){
+		for(;;);
+	}
 }
