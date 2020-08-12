ef66e32e49c9f50c8303e56349177d56e9df952b
AdeRegt <AdeRegt@github.com>
2020-08-12 23:07:18 +0200

    adding new fasm program

---
+++ include/SanderOSUSB.h
@@ -0,0 +1,54 @@
+#define STDIO 1
+#define NULL 0
+
+void exit (void){
+	int mode = 1;
+	__asm__ __volatile__ (
+        "int $0x80"
+        : "+a" (mode)
+    	);
+}
+
+void print(const char* message,int to){
+	int mode = 4;
+	int len = 0;
+	while(message[len++]!=NULL);
+	__asm__ __volatile__ (
+        "int $0x80"
+        : "+a" (mode)
+        , "+b" (to)
+        , "+c" (message)
+        , "+d" (len)
+    	);
+}
+
+void read(int to,char *message,int len){
+	int mode = 3;
+	__asm__ __volatile__ (
+        "int $0x80"
+        : "+a" (mode)
+        , "+b" (to)
+        , "+c" (message)
+        , "+d" (len)
+    	);
+}
+
+int open(const char *pathname, int flags, int mode){
+	int model = 5;
+	int res = 0;
+	__asm__ __volatile__ (
+        "int $0x80"
+        : "+a" (model)
+        , "+b" (pathname)
+        , "+c" (flags)
+        , "+d" (mode)
+        :
+        "r" (res)
+    	);
+    	return res;
+}
+
+void printf(const char* msg,...){
+    void* (*foo)(const char*,...) = (void*) 0x100012;
+	foo(msg);
+}
