a615f0c5e9de2f12983ed2674dda2e968e53d69d
AdeRegt <AdeRegt@github.com>
2020-09-10 19:24:04 +0200

    adding basic efi support

--- kernel/build64.sh
+++ kernel/build64.sh
@@ -1,1 +1,18 @@
 echo "64-bit kernel building"
+
+x86_64-w64-mingw32-gcc -ffreestanding -I/usr/include/efi -I/usr/include/efi/x86_64 -I/usr/include/efi/protocol -c -o uefi.o stub/uefi/uefi.c || exit
+x86_64-w64-mingw32-gcc -nostdlib -Wl,-dll -shared -Wl,--subsystem,10 -e efi_main -o ../KERNEL64.EFI uefi.o -lgcc || exit # data.o 
+
+rm *.o
+
+dd if=/dev/zero of=fat.img bs=1k count=1440
+mformat -i fat.img -f 1440 ::
+mmd -i fat.img ::/EFI
+mmd -i fat.img ::/EFI/BOOT
+mcopy -i fat.img ../KERNEL64.EFI ::/EFI/BOOT
+
+mkdir iso
+cp fat.img iso
+xorriso -as mkisofs -R -f -e fat.img -no-emul-boot -o ../cdrom.iso iso
+rm fat.img
+rm -r -f iso
---
+++ kernel/stub/uefi/uefi.c
@@ -0,0 +1,34 @@
+#include <efi.h>
+#include <efilib.h>
+ 
+EFI_SYSTEM_TABLE *ST;
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+    EFI_STATUS Status;
+    EFI_INPUT_KEY Key;
+ 
+    /* Store the system table for future use in other functions */
+    ST = SystemTable;
+ 
+    /* Say hi */
+    Status = ST->ConOut->OutputString(ST->ConOut, L"SanderOSUSB has been loaded\n\r");
+    if (EFI_ERROR(Status))
+        return Status;
+ 
+    /* Now wait for a keystroke before continuing, otherwise your
+       message will flash off the screen before you see it.
+ 
+       First, we need to empty the console input buffer to flush
+       out any keystrokes entered before this point */
+    Status = ST->ConIn->Reset(ST->ConIn, FALSE);
+    if (EFI_ERROR(Status))
+        return Status;
+ 
+    /* Now wait until a key becomes available.  This is a simple
+       polling implementation.  You could try and use the WaitForKey
+       event instead if you like */
+    while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ;
+ 
+    return Status;
+}
