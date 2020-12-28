5b69bfc3548b1c305fbe28850112d64173442ec2
AdeRegt <AdeRegt@github.com>
2020-12-28 22:14:59 +0100

    adding possibility to show compiled time

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -82,6 +82,7 @@ void kernel_main(){
 	init_soundblaster16();
 	printf("Shashwat %d sss %s",1, "test2");
 	printstring("\nEnd of loading system!\n");
+	printf("The system is compiled at %s %s \n",__DATE__,__TIME__);
 	
 	if(init_graph_vga(320, 200, 1)==0) {
 		printf("VGA: failed to set!\nPress any key to reboot\n");
