d28cd2d09ebcdc7638b2beaeb7db1640d1098463
Sander de Regt <sander@snd1.home>
2021-07-18 11:26:20 +0200

    adding to virtualbox debugging

--- kernel/dev/vbox.c
+++ kernel/dev/vbox.c
@@ -7,6 +7,7 @@
 #define VBOX_REQUEST_GET_MOUSE 1
 #define VBOX_REQUEST_SET_MOUSE 2
 #define VBOX_REQUEST_GUEST_INFO 55
+#define VBOX_REPORT_GUEST_INFO 50
 
 typedef struct {
         unsigned long size;
@@ -26,8 +27,8 @@ typedef struct {
 typedef struct  {
         vbox_header header;
         unsigned long features;
-        long x;
-        long y;
+        unsigned long x;
+        unsigned long y;
 } vbox_mouse_absolute; 
  
 
@@ -37,10 +38,27 @@ extern void vbirq();
 
 void irq_vb(){
 	outportb(0x20,0x20);
-	debugf("VB:DDDD\n");
+	printf("VB:DDDD\n");
+}
+
+unsigned char is_vbox = 0;
+
+void writer_string_vbox(char *message){
+        for(int i = 0 ; i < strlen(message) ; i++){
+                outportb(0x504,message[i]);
+        }
+}
+
+unsigned char is_virtual_box_session_enabled(){
+        return is_vbox;
+}
+
+void set_virtual_box_session_enabled(unsigned char a){
+        is_vbox = a;
 }
 
 void init_vbox(unsigned long bus, unsigned long slot, unsigned long function){
+        set_virtual_box_session_enabled(1);
 	debugf("[VBOX] Virtualbox Guest Additions found!\n");
 	for(int i = 0 ; i < 6 ; i++){
 		unsigned long addr = getBARaddress(bus,slot,function,0x10+(i*4));
@@ -49,5 +67,8 @@ void init_vbox(unsigned long bus, unsigned long slot, unsigned long function){
 		}
 	}
 	vbox_port = getBARaddress(bus,slot,function,0x10) & 0xFFFFFFF0;
-	debugf("[VBOX] Default port is %x with default value %x \n",vbox_port,inportl(vbox_port));
+        unsigned long vbox_int = getBARaddress(bus,slot,function,0x3C) & 0x000000FF;
+	debugf("[VBOX] Default port is %x with int %x enabled virtualbox logging \n",vbox_port,vbox_int);
+        setNormalInt(vbox_int,(unsigned long)vbirq);
+
 }
